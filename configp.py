77df40ff0a45c948469e3bda34ef2c1742057e0a
Yanteng <50645005+yntn32@users.noreply.github.com>
2026-01-30 01:24:13 -0500

    Update configp.py

--- configp.py
+++ configp.py
@@ -4,19 +4,6 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--AD_dir', type=str,
-                        help='subfloder of train or test dataset', default='AD/')
-    parser.add_argument('--CN_dir', type=str,
-                        help='subfloder of train or test dataset', default='CN/')
-    parser.add_argument('--MCI_dir', type=str,
-                        help='subfloder of train or test dataset', default='MCI/')
-    parser.add_argument('--PMCI_dir', type=str,
-                        help='subfloder of train or test dataset', default='PMCI/')
-    parser.add_argument('--SMCI_dir', type=str,
-                        help='subfloder of train or test dataset', default='SMCI/')
-
-    # parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-    #                     help='learning rate (default: 0.0001)')
     parser.add_argument('--class_num', type=int, help='class_num', default=2)
     parser.add_argument('--seed', type=int, help='Seed', default=42)
     parser.add_argument('--gpu', type=str, help='GPU ID', default='0')
@@ -33,5 +20,5 @@ def get_args():
     parser.add_argument('--val_root_path_MRI', type=str, help='Root path for val dataset',
                         default='D:/PET_MRI/dataset/dataset14/mribrfiv14/val/')
     parser.add_argument('--batch_size', type=int, help='batch_size of data', default=6)
-    parser.add_argument('--nepoch', type=int, help='Total epoch num', default=69)
+    parser.add_argument('--nepoch', type=int, help='Total epoch num', default=80)
     return parser.parse_args()
